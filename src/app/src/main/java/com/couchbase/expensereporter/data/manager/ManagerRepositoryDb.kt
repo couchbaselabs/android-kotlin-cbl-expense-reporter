eb105370020028a71880871285381ee3584cca60
Aaron LaBeau <alabeau@gmail.com>
2022-10-31 23:52:59 -0500

    updated with implementation

--- src/app/src/main/java/com/couchbase/expensereporter/data/manager/ManagerRepositoryDb.kt
+++ src/app/src/main/java/com/couchbase/expensereporter/data/manager/ManagerRepositoryDb.kt
@@ -3,7 +3,6 @@ package com.couchbase.expensereporter.data.manager
 import android.content.Context
 import android.util.Log
 import com.couchbase.lite.*
-import com.couchbase.lite.Function
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.decodeFromString
@@ -26,16 +25,77 @@ class ManagerRepositoryDb(context: Context)
     }
 
     override suspend fun getByDepartmentTitle(department: String, title: String?): List<Manager> {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            var items = mutableListOf<Manager>()
+            try {
+                val db = databaseProvider.startingDatabase
+                db?.let { database ->
+                    var queryString = "SELECT * FROM _ AS item WHERE documentType=\"manager\" AND department=\$department"
+
+                    val parameters = Parameters()  // <2>
+                    parameters.setValue("department", department)
+
+                    title?.let { // <3>
+                        queryString += " AND title=\$title"
+                        parameters.setValue("title", it)
+                    }
+
+                    val query = database.createQuery(queryString) // <4>
+                    query.parameters = parameters; // <5>
+
+                    query.execute()
+                        .allResults()
+                        .forEach { item ->  // <6>
+                            val json = item.toJSON()
+                            val manager = Json.decodeFromString<ManagerDao>(json).item
+                            items.add(manager)
+                        }
+                }
+            } catch (e: Exception) {
+                Log.e(e.message, e.stackTraceToString())
+            }
+            return@withContext items
+        }
     }
 
     override suspend fun get(): List<Manager> {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            var items = mutableListOf<Manager>()
+            try {
+                val db = databaseProvider.startingDatabase
+                db?.let { database ->
+                    database
+                        .createQuery("SELECT * FROM _ AS item WHERE documentType=\"manager\"")
+                        .execute()
+                        .allResults()
+                        .forEach { item -> //<2>
+                            val json = item.toJSON()
+                            val manager = Json.decodeFromString<ManagerDao>(json).item
+                            items.add(manager)
+                        }
+                }
+            } catch (e: Exception) {
+                Log.e(e.message, e.stackTraceToString())
+            }
+            return@withContext items
+        }
     }
 
     override suspend fun count(): Int {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            var count = 0
+            try {
+                val db = databaseProvider.startingDatabase
+                db?.let { database ->
+                    val query =
+                        database.createQuery("SELECT COUNT(*) AS count FROM _ AS item WHERE documentType=\"manager\"") // <1>
+                    val results = query.execute().allResults() // <2>
+                    count = results[0].getInt("count") // <3>
+                }
+            } catch (e: Exception) {
+                Log.e(e.message, e.stackTraceToString())
+            }
+            return@withContext count
+        }
     }
-
-
 }
