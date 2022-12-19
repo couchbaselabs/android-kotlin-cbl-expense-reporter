d89463db3e717fb07ca6d441abc7f63869f8b98f
Aaron LaBeau <alabeau@gmail.com>
2022-12-19 13:46:41 -0600

    fix for join statement

--- src/app/src/main/java/com/couchbase/expensereporter/data/report/ReportRepositoryDb.kt
+++ src/app/src/main/java/com/couchbase/expensereporter/data/report/ReportRepositoryDb.kt
@@ -60,39 +60,8 @@ class ReportRepositoryDb(
             try {
                 val db = databaseProvider.reportDatabase
                 db?.let { database ->
-                    //QUERY WITHOUT JOIN
-                    val query = database.createQuery("SELECT item.reportId, item.name, item.description, item.isComplete, item.documentType, item.reportDate, item.updatedDate, item.status, item.department, item.createdBy, item.approvalManager, 0.0 as amount FROM _ AS item WHERE item.documentType=\"report\"")
-
-                  /* QUERYBUIDER VERSION OF JOIN
-                    val query = QueryBuilder.select(
-                        SelectResult.expression(Expression.property("reportId").from("item")),
-                        SelectResult.expression(Expression.property("name").from("item")),
-                        SelectResult.expression(Expression.property("description").from("item")),
-                        SelectResult.expression(Expression.property("isComplete").from("item")),
-                        SelectResult.expression(Expression.property("documentType").from("item")),
-                        SelectResult.expression(Expression.property("reportDate").from("item")),
-                        SelectResult.expression(Expression.property("updatedDate").from("item")),
-                        SelectResult.expression(Expression.property("status").from("item")),
-                        SelectResult.expression(Expression.property("department").from("item")),
-                        SelectResult.expression(Expression.property("createdBy").from("item")),
-                        SelectResult.expression(Expression.property("approvalManager").from("item")),
-                        SelectResult.expression(Function.sum(Expression.property("amount").from("e"))).`as`("amount")
-                        )
-                        .from(DataSource.database(database).`as`("item"))
-                        .join(Join.join(DataSource.database(database).`as`("e"))
-                            .on(Expression.property("reportId").from("item").equalTo(Expression.property("reportId").from("e"))))
-                        .where(
-                            Expression.property("documentType").from("item")
-                                .equalTo(Expression.string("report"))
-                                .and(
-                                    Expression.property("documentType").from("e")
-                                        .equalTo(Expression.string("expense"))
-                                )
-                        )
-                    */
-
                     //SQL++ VERSION OF THE JOIN
-                    //val query = database.createQuery("SELECT item.reportId, item.name, item.description, item.isComplete, item.documentType, item.reportDate, item.updatedDate, item.status, item.department, item.createdBy, item.approvalManager, SUM(e.amount) as amount FROM _ AS item LEFT JOIN _ AS e ON item.reportId = e.reportId WHERE item.documentType=\"report\" AND e.documentType=\"expense\"")
+                    val query = database.createQuery("SELECT item.reportId, item.name, item.description, item.isComplete, item.documentType, item.reportDate, item.updatedDate, item.status, item.department, item.createdBy, item.approvalManager, SUM(e.amount) as amount FROM _ AS item LEFT OUTER JOIN _ AS e ON item.reportId = e.reportId AND e.documentType = \"expense\" WHERE item.documentType=\"report\" GROUP BY item.reportId, e.reportId, item.name, item.description, item.isComplete, item.documentType, item.reportDate, item.updatedDate, item.status, item.department, item.createdBy, item.approvalManager")
 
                     // create a flow to return the results dynamically as needed - more information on
                     // CoRoutine Flows can be found at
